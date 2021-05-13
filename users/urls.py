aa10f508c3a698ddf6736452c25e5cec06c18035
lookin44 <look-in44@yandex.ru>
2021-05-13 13:32:47 +0300

    Change reset email

--- users/urls.py
+++ users/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('signup/', views.SignUp.as_view(), name='signup'),
+    path('password_reset/', views.MyPasswordResetView.as_view(), name='password_reset'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.views import PasswordResetView
+from django.http import HttpResponseRedirect
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
 from .forms import CreationForm
@@ -7,3 +9,21 @@ class SignUp(CreateView):
     form_class = CreationForm
     success_url = reverse_lazy("login")
     template_name = "signup.html"
+
+
+class MyPasswordResetView(PasswordResetView):
+
+    def form_valid(self, form):
+        opts = {
+            'use_https': self.request.is_secure(),
+            'token_generator': self.token_generator,
+            'from_email': self.from_email,
+            'email_template_name': self.email_template_name,
+            'subject_template_name': self.subject_template_name,
+            'request': self.request,
+            'html_email_template_name': 'registration/password_reset_email.html',
+            'extra_email_context': self.extra_email_context,
+        }
+        form.save(**opts)
+        return HttpResponseRedirect(self.get_success_url())
+
